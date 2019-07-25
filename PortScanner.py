321d11a34f8d0fe2c2518763b371e874f7775aac
Sameh <40921921+samehness@users.noreply.github.com>
2019-07-25 09:45:08 +0300

    Add files via upload

---
+++ PortScanner.py
@@ -0,0 +1,17 @@
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+target = raw_input("[+] Enter Target IP: ")
+
+def scanner (port):
+    try:
+        sock.connect((target, port))
+        return True
+    except:
+        return False
+
+
+for portNumber in range(1, 100):
+    print("Scanning port", portNumber)
+    if scanner(portNumber):
+        print("[*] Port", portNumber, "/tcp", "is open")
